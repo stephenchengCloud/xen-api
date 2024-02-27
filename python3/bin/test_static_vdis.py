b3d842afe6a6a85cb31c5f026ea43bc5c2bc419a
Stephen Cheng <stephen.cheng@cloud.com>
2024-02-27 05:41:54 +0000

    CP-48201: Create a subdirectory for python3-only scripts insatlled in BIN
    Signed-off-by: Stephen Cheng <stephen.cheng@cloud.com>

--- python3/bin/test_static_vdis.py
+++ python3/bin/test_static_vdis.py
@@ -6,7 +6,6 @@ import unittest
 from mock import MagicMock
 import sys
 import os
-import subprocess
 import tempfile
 
 # mock modules to avoid dependencies
@@ -36,19 +35,18 @@ def get_module():
 
 static_vdis = get_module()
 
-@unittest.skipIf(sys.version_info < (3, 0), reason="requires python3")     
 class TestReadWriteFile(unittest.TestCase):
     def test_write_and_read_whole_file(self):
         """Test read_whole_file and write_whole_file"""
         test_file = tempfile.NamedTemporaryFile(delete=True)
         filename = str(test_file.name)
-        content = r"""def read_whole_file(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        return ''.join(f.readlines()).strip()
-        
-def write_whole_file(filename, contents):
-    with open(filename, "w", encoding='utf-8') as f:
-        f.write(contents)"""
+        content = r"""<?xml version="1.0" ?>
+<sequenceresults>
+  <name>storagenetwork</name>
+  <jobdetails>
+    <host>cl14-03</host>
+    <host>cl14-01</host>
+  </jobdetails>"""
         static_vdis.write_whole_file(filename, content)
         expected_content = static_vdis.read_whole_file(filename)
         self.assertEqual(expected_content, content)
